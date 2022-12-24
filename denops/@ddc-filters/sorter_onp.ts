916cc018cb18b74999904c62391c3f67ca898387
kuuote <znmxodq1@gmail.com>
2022-12-24 14:15:34 +0900

    Update dependencies

--- denops/@ddc-filters/sorter_onp.ts
+++ denops/@ddc-filters/sorter_onp.ts
@@ -2,23 +2,23 @@
 // This work is licensed under the MIT License. https://git.io/mit-license
 
 import onp from "https://esm.sh/onp@2";
-import { Candidate } from "https://lib.deno.dev/x/ddc_vim@v0/types.ts";
+import { Item } from "https://lib.deno.dev/x/ddc_vim@v3/types.ts";
 import {
   BaseFilter,
   FilterArguments,
-} from "https://lib.deno.dev/x/ddc_vim@v0/base/filter.ts";
+} from "https://lib.deno.dev/x/ddc_vim@v3/base/filter.ts";
 
 type Params = Record<string, never>;
 
 export class Filter extends BaseFilter<Params> {
-  override filter(args: FilterArguments<Params>): Promise<Candidate[]> {
-    const matches = new Map<Candidate, number>(
-      args.candidates.map((candidate) => [
-        candidate,
-        onp.diffText(args.completeStr, candidate.word).distance
+  override filter(args: FilterArguments<Params>): Promise<Item[]> {
+    const matches = new Map<Item, number>(
+      args.items.map((item) => [
+        item,
+        onp.diffText(args.completeStr, item.word).distance
       ])
     )
-    return Promise.resolve(args.candidates.sort((a, b) => {
+    return Promise.resolve(args.items.sort((a, b) => {
       return (matches.get(a) ?? 0) - (matches.get(b) ?? 0)
     }));
   }
